eaaaffb1e589a73181b10d778ec87ead9d612185
tom0334 <tfriederich@live.nl>
2019-05-31 16:49:55 +0200

    Add bottom bar

--- app/src/main/java/com/tobo/huiset/MainActivity.kt
+++ app/src/main/java/com/tobo/huiset/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tobo.huiset
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupBottomTabs()
+    }
+
+    private fun setupBottomTabs(){
+        val bottomView  = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomView.inflateMenu(R.menu.menu_bottom_navigation)
     }
 }
+
+        val bottomView  = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomView.inflateMenu(R.menu.menu_bottom_navigation)
