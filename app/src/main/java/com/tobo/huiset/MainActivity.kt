26d85a8f617ee6ecf8a6fcd2aede0b75b97b35a7
bovandenberg <B.vandenBerg-6@student.tudelft.nl>
2019-05-31 17:48:29 +0200

    Added Settings and Achievements to Action Bar

--- app/src/main/java/com/tobo/huiset/MainActivity.kt
+++ app/src/main/java/com/tobo/huiset/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.tobo.huiset
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import android.view.MenuItem
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,4 +14,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // create an action bar button
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // handle button activities
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.getItemId()
+
+        if (id == R.id.settings) {
+            Toast.makeText(this, "settings clicked", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        if (id == R.id.achievements) {
+            Toast.makeText(this, "achievements clicked", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, AchievementsActivity::class.java)
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
