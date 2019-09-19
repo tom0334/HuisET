e292e22ee4ef929db779ebd1155f437c1e0d5ece
bovandenberg <B.vandenBerg-6@student.tudelft.nl>
2019-09-19 22:00:21 +0200

    setcontentview

--- app/src/main/java/com/tobo/huiset/gui/activities/TransferMoneyActivity.kt
+++ app/src/main/java/com/tobo/huiset/gui/activities/TransferMoneyActivity.kt
@@ -1,7 +1,13 @@
 package com.tobo.huiset.gui.activities
 
+import android.os.Bundle
+import com.tobo.huiset.R
 import com.tobo.huiset.extendables.HuisEtActivity
 
 class TransferMoneyActivity : HuisEtActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transfermoney)
+    }
 }
