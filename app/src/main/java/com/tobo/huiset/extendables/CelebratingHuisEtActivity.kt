6d969f2f6506717b713db18fb7c4e6d76be7137b
Tom Friederich <tfriederich@live.nl>
2019-12-02 14:24:25 +0100

    Fix the confetti location

--- app/src/main/java/com/tobo/huiset/extendables/CelebratingHuisEtActivity.kt
+++ app/src/main/java/com/tobo/huiset/extendables/CelebratingHuisEtActivity.kt
@@ -5,6 +5,8 @@ import android.widget.Toast
 import com.tobo.huiset.R
 import com.tobo.huiset.achievements.AchievementManager
 import com.tobo.huiset.realmModels.AchievementCompletion
+import com.tobo.huiset.utils.extensions.getDisplayHeight
+import com.tobo.huiset.utils.extensions.getDisplayWith
 import nl.dionsegijn.konfetti.KonfettiView
 import nl.dionsegijn.konfetti.models.Shape
 import nl.dionsegijn.konfetti.models.Size
@@ -28,7 +30,7 @@ abstract class CelebratingHuisEtActivity: HuisEtActivity(){
                 .setTimeToLive(2000L)
                 .addShapes(Shape.CIRCLE)
                 .addSizes(Size(12))
-                .setPosition(viewKonfetti.width / 2f, viewKonfetti.height / 2f)
+                .setPosition(getDisplayWith() / 2f, getDisplayHeight()/ 2f)
                 .burst(300)
         }
     }
